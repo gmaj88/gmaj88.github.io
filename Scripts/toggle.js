ba5d35f43b971daa0fa82b907aef53467db46537
gmaj <gmaj88@163.com>
2019-07-28 22:25:56 +0800

    'add-png'

---
+++ Scripts/toggle.js
@@ -0,0 +1,33 @@
+$(function(){
+	var h=$(window).height();
+	var w=$(window).width();
+	var t=(h-720)/2+"px";
+	var center=$('#center');
+	if(h>720){
+		center.animate({top:t},2000);
+	}else{
+		center.animate({top:'0px'},2000);
+	};
+	if(h<w){ setTimeout(function(){center.addClass('rotate_X')},2500)};
+	$('body').mouseenter(function(){
+    	center.css('transition','transform 1s');
+	});
+	center.hover(function(){
+		center.css('transition','transform 3s');
+	},function(){
+		center.css('transition','transform 1s');
+	});	
+	$('img').hover(function(){
+		 $(this).stop().animate({height:'60%',width:'60%'},500);
+	},function(){
+		 $(this).stop().animate({height:'73%',width:'73%'},500);
+	});
+});
+function xingshi(div_id,div_height){
+	$('#works_a').css({'bottom':'-1500px','height':'0px'});
+	$('#works_b').css({'bottom':'-1500px','height':'0px'});
+	$('#works_c').css({'bottom':'-1500px','height':'0px'});
+	$('#works_d').css({'bottom':'-1500px','height':'0px'});
+	$(div_id).animate({bottom:'0px',height:div_height},1000);	
+	$('#center').css({'top':'0px','transform':'rotateX(0deg)'});
+}
